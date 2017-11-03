a57b7d1788f27a55511f80dcba6e6b8524792c3a
oh000024 <jakeohcs@gmail.com>
2017-11-02 22:59:54 -0400

    [MOD] Delete a comment out

--- src/index.js
+++ src/index.js
@@ -1,43 +1,12 @@
 
-//import Title from "./Title";
+
 var Title = require("./Title.js");
-
 var ProfileDetail = require("./detailprofile.js");
-
-// var ContactItem = require("./contactItem.js");
-
 var ProfileList = require("./profilelist.js")
 
 require("./main.scss"); 
 
 const _contacts = require("../data/contacts").contacts;
-
-
-// let ContactItem = React.createClass({
-//     propTypes:{
-//         id: React.PropTypes.number,
-//         picture:React.PropTypes.object.isRequired,
-//         name: React.PropTypes.object.isRequired,
-//         dob: React.PropTypes.string.isRequired,
-//         onClick: React.PropTypes.func.isRequired
-//     },
-//     render: function(){
-//         return (
-//             React.createElement("li",{className:"contact",onClick:this.props.onClick,id:"contact-"+this.props.id},
-//                 React.createElement("div",{className:"row"},
-//                    React.createElement("div",{className:"column pic"},
-//                        React.createElement("img",{src: this.props.picture.thumbnail})),
-//                     React.createElement("div",{className:"column data"},this.props.name.first+" "+this.props.name.last,
-//                     React.createElement("br"),
-//                     React.createElement("p",{},this.props.dob.split(" ")[0]))
-//                 )
-//             )
-//         )
-//     }
-// });
-
-
-
 
 let Main  = React.createClass({
     propTypes:{
@@ -57,6 +26,7 @@ let Main  = React.createClass({
         );
     }
 })  
+
 var state={};
 var contactList = _contacts;
 var isSelect=true;
